87b335c985ff72a8aed978e4f60bfac9f0da3399
Brian Schuster <brian.daniel.schuster@gmail.com>
2018-01-25 15:26:29 -0500

    Altered Data Definition to include more specifics on type

--- Datamart/dev/creation.sql
+++ Datamart/dev/creation.sql
@@ -2,17 +2,16 @@
 CREATE TABLE factTransactions
 (
   id int,
-  date timestamp,
-  unixdate int,
+  datetime timestamp,
+  unixtimestamp int,
   transaction_hash VARCHAR(256),
   sender_address VARCHAR(256),
   transaction_type_name VARCHAR(40),
-  transaction_subtype_name VARCHAR(40),
   fee decimal(20,10),
-  blockchain_network_name int,
-  sent_currency_name int,
+  blockchain_network_name VARCHAR(40),
+  sent_currency_name VARCHAR(40),
   sent_currency_amount decimal(20,10),
-  exchange_received_currency_name int,
-  exchange_received_amount int,
+  exchange_received_currency_name VARCHAR(40),
+  exchange_received_amount decimal(20,10),
   exchange_rate VARCHAR(40)
 );
