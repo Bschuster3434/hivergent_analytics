a6f2bcfe9b558cab7389816be48e99ae4c8c77d4
Brian Schuster <brian.daniel.schuster@gmail.com>
2018-03-04 13:38:09 -0500

    Updated creation script to change decimal to 30/15 length

--- Datamart/dev/creation.sql
+++ Datamart/dev/creation.sql
@@ -8,12 +8,12 @@ CREATE TABLE factTransactions
   sender_address VARCHAR(256),
   transaction_type_name VARCHAR(40),
   transaction_subtype_name VARCHAR(40),
-  fee decimal(20,10),
+  fee decimal(30,15),
   blockchain_network_name VARCHAR(40),
   sent_currency_name VARCHAR(40),
-  sent_currency_amount decimal(20,10),
+  sent_currency_amount decimal(30,15),
   exchange_received_currency_name VARCHAR(40),
-  exchange_received_amount decimal(20,10),
+  exchange_received_amount decimal(30,15),
   exchange_rate VARCHAR(40),
   status int
 );
