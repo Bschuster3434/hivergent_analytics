be75818ce7aeb6ffbe5673937c7be60cd27a6533
Brian Schuster <brian.daniel.schuster@gmail.com>
2018-03-23 12:09:47 -0400

    Added additional sample select statements

--- Datamart/dev/sample_select_statements.sql
+++ Datamart/dev/sample_select_statements.sql
@@ -1,14 +1,27 @@
+-- Standard Select
 SELECT *
  FROM dev.public.facttransactions LIMIT 100;
 
+ --See Individual Blokchain Network Names
  SELECT *
  FROM dev.public.facttransactions
  WHERE blockchain_network_name = 'bitcoin'
  LIMIT 100;
 
+ --Transaction Type by Network
  SELECT blockchain_network_name,
  transaction_type_name,
  count(*)
  FROM dev.public.facttransactions
  Group By blockchain_network_name,
  transaction_type_name;
+
+ --- Currencies by Network
+SELECT blockchain_network_name,
+sent_currency_name,
+count(*)
+FROM dev.public.facttransactions
+where sent_currency_name != ''
+Group By blockchain_network_name,
+sent_currency_name
+;
