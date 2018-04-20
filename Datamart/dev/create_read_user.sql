6f6f5c6a8ee98deea87d2b90b58a49ebf1de2cbb
Brian Schuster <brian.daniel.schuster@gmail.com>
2018-04-20 13:21:01 -0400

    moved a bunch of files

--- Datamart/dev/create_read_user.sql
+++
@@ -1,6 +0,0 @@
-create user hivergent_betauser password 'BetaUser1';
-grant select on all tables in schema public to hivergent_beta;
-GRANT SELECT ON TABLE public.currencytoudsconversion TO hivergent_betauser;
-GRANT SELECT ON TABLE public.facttransactions TO hivergent_betauser;
-GRANT SELECT ON TABLE public.facttransactionsusd TO hivergent_betauser;
-GRANT SELECT ON TABLE public.smartcontractdetails TO hivergent_betauser;
--- Datamart/dev/view_to_pipe_unload.sql
+++
@@ -1,4 +0,0 @@
-unload ('select * from public.facttransactionsusd')
-to 's3://hivergent-analytics/dev/table_export/'
-CREDENTIALS 'aws_iam_role=arn:aws:iam::485025720738:role/redshift_s3_lambda_admin'
-maxfilesize 100 mb;
