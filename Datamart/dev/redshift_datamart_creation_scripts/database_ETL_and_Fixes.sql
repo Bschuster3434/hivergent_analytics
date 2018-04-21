932ff876bba0aea8b61c81f28f5be8e71e83a275
Brian Schuster <brian.daniel.schuster@gmail.com>
2018-04-21 10:42:27 -0400

    Added small tweak to dataset to eliminate 18 records from Ripple for the day after the selected timeframe

--- Datamart/dev/redshift_datamart_creation_scripts/database_ETL_and_Fixes.sql
+++ Datamart/dev/redshift_datamart_creation_scripts/database_ETL_and_Fixes.sql
@@ -56,7 +56,7 @@ INSERT INTO dev.v2.factCrossTransactionsTable (
 
 --xrp
 INSERT INTO dev.v2.factCrossTransactionsTable (
-  SELECT
+  SELECT DISTINCT
   a.id,
   a.datetime as "gmt_datetime",
   a.unixtimestamp,
@@ -105,7 +105,7 @@ INSERT INTO dev.v2.factCrossTransactionsTable (
   a.sent_currency_amount ,
   (CAST(a.sent_currency_amount AS DECIMAL(24,6)) * CAST(c.usd_value AS DECIMAL(24,6))) as "sent_currency_amount_usd",
   a.status
-  FROM dev.v2.stageRippleTransactions a
+  FROM dev.v2.stageethereumtransactions a
   LEFT OUTER JOIN (
     SELECT currency_name, usd_value, currency_type
     FROM dev.v2.dimCurrency
@@ -118,34 +118,37 @@ INSERT INTO dev.v2.factCrossTransactionsTable (
     WHERE network_name = 'ethereum' ) c ON a.sent_currency_name = c.currency_name
 );
 
+DELETE FROM v2.factcrosstransactionstable
+WHERE unixtimestamp = 1514764800;
+
 --factEthereumTransactionsTable
 INSERT INTO dev.v2.factEthereumTransactionsTable (
   SELECT
-  a.id,
-  a.datetime as "gmt_datetime",
-  a.unixtimestamp,
-  a.transaction_hash
-  a.sender_address,
-  a.transaction_type_name,
-  a.transaction_function_name,
-  a.fee,
-  (CAST(a.fee AS DECIMAL(24,6)) * CAST(b.usd_value AS DECIMAL(24,6))) as "fee_usd",
-  a.fee_currency_name,
-  a.blockchain_network_name,
-  c.currency_type as "sent_currency_type",
-  a.sent_currency_name ,
-  a.sent_currency_amount ,
-  (CAST(a.sent_currency_amount AS DECIMAL(24,6)) * CAST(c.usd_value AS DECIMAL(24,6))) as "sent_currency_amount_usd",
-  a.status
-  FROM dev.v2.stageEthereumTransactions a
-  LEFT OUTER JOIN (
-    SELECT currency_name, usd_value, currency_type
-    FROM dev.v2.dimCurrency
-    WHERE currency_type = 'native_asset'
-    and network_name = 'ethereum'
-    and currency_name = 'ETH' ) b ON a.fee_currency_name = b.currency_name
-  LEFT OUTER JOIN (
-    SELECT currency_name, usd_value, currency_type
-    FROM dev.v2.dimCurrency
-    WHERE network_name = 'ethereum' ) c ON a.sent_currency_name = c.currency_name
+   a.id,
+   a.datetime as "gmt_datetime",
+   a.unixtimestamp,
+   a.transaction_hash,
+   a.sender_address,
+   a.transaction_type_name,
+   a.transaction_function_name,
+   a.fee,
+   (CAST(a.fee AS DECIMAL(24,6)) * CAST(b.usd_value AS DECIMAL(24,6))) as "fee_usd",
+   a.fee_currency_name,
+   a.blockchain_network_name,
+   c.currency_type as "sent_currency_type",
+   a.sent_currency_name ,
+   a.sent_currency_amount ,
+   (CAST(a.sent_currency_amount AS DECIMAL(24,6)) * CAST(c.usd_value AS DECIMAL(24,6))) as "sent_currency_amount_usd",
+   a.status
+   FROM dev.v2.stageEthereumTransactions a
+   LEFT OUTER JOIN (
+     SELECT currency_name, usd_value, currency_type
+     FROM dev.v2.dimCurrency
+     WHERE currency_type = 'native_asset'
+     and network_name = 'ethereum'
+     and currency_name = 'ETH' ) b ON a.fee_currency_name = b.currency_name
+   LEFT OUTER JOIN (
+     SELECT currency_name, usd_value, currency_type
+     FROM dev.v2.dimCurrency
+     WHERE network_name = 'ethereum' ) c ON a.sent_currency_name = c.currency_name
 );
